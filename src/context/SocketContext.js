8b8ac799843cb5def54879bf973cca8ffca08e1d
Fecso <Fecsoul@gmail.com>
2022-09-22 00:18:56 -0300

    Deploy test

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -5,7 +5,7 @@ export const SocketContext = createContext();
 
 export const SocketProvider = ( { children }) => {
 
-    const { socket, online } = useSocket('http://localhost:8080'); 
+    const { socket, online } = useSocket('https://serversocketkant.netlify.app'); 
 
     return (
         <SocketContext.Provider value={{ online,socket }}>
